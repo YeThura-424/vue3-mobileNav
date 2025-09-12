1e876d6c2f955e9ca25b7978023b13bef780414c
YeThura-424 <rathanpoudle.26@gmail.com>
2025-09-12 22:11:12 +0630

    update for style

--- src/components/AnimatedNav.vue
+++ src/components/AnimatedNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative w-[360px] h-[48px] bg-white flex items-center justify-center px-5">
+  <div class="relative w-[360px] h-[58px] bg-white flex items-center justify-center px-5">
     <ul class="flex justify-between w-full relative">
       <li v-for="(item, index) in navItems" :key="index"
         class="relative w-[70px] h-[70px] z-10 flex flex-col items-center justify-center cursor-pointer"
@@ -8,14 +8,14 @@
           <!-- Tabler Icon -->
           <component :is="item.icon" class="transition-all duration-500 w-[22px] h-[22px]" :class="[
             activeIndex === index
-              ? 'text-[#7F3DFF] -translate-y-8'
+              ? 'text-[#7F3DFF] -translate-y-7 w-[26px] h-[26px]'
               : 'text-gray-900 translate-y-0',
           ]" />
 
           <!-- Text -->
           <span class="absolute transition-all duration-500 text-gray-900 text-sm" :class="[
             activeIndex === index
-              ? 'opacity-100 translate-y-2'
+              ? 'opacity-100 translate-y-4'
               : 'opacity-0 translate-y-5',
           ]">
             {{ item.text }}
@@ -24,11 +24,11 @@
       </li>
 
       <!-- Moving Indicator -->
-      <li class="absolute top-[10.5px] left-[-22px] transition-transform duration-300 z-0"
+      <li class="absolute top-[6px] left-[-22px] transition-transform duration-300 z-0"
         :style="{ transform: `translateX(${indicatorX}px)` }">
         <Curve />
       </li>
-      <li class="absolute top-[-14px] left-[7px] transition-transform duration-300 z-0"
+      <li class="absolute top-[-15px] left-[8px] transition-transform duration-300 z-0"
         :style="{ transform: `translateX(${indicatorX}px)` }">
         <Circle />
       </li>
@@ -61,6 +61,6 @@ function setActive(index) {
 }
 
 const indicatorX = computed(() => {
-  return activeIndex.value * 82; // 70px width + ~12px spacing
+  return activeIndex.value + activeIndex.value * 82; // 70px width + ~12px spacing
 });
 </script>
